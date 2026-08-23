5336ba9c41717cddf3045964836737129df60490
Kurogenshi <rouffet.julien@outlook.fr>
2026-08-24 00:16:42 +0200

    Fix stb build

--- external/stb-cmake/stb_implementation.cpp
+++ external/stb-cmake/stb_implementation.cpp
@@ -1,10 +1,8 @@
 #define STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_STATIC
 #define STBI_FAILURE_USERMSG
 
 #include <stb_image.h>
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#define STB_IMAGE_WRITE_STATIC
 
 #include <stb_image_write.h>
