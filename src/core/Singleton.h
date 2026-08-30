040e68d75894567bf85e710806ed072be6c0a128
Kurogenshi <rouffet.julien@outlook.fr>
2026-08-30 18:38:42 +0200

    Add Singleton class

---
+++ src/core/Singleton.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace sandbox {
+	template <typename T>
+	class Singleton {
+	public:
+		static T& Instance() {
+			static T instance;
+			return instance;
+		}
+
+		Singleton(const Singleton&) = delete;
+		Singleton& operator&(const Singleton&) = delete;
+
+	protected:
+		Singleton() = default;
+	};
+}
